f7a337d8b735fd8f14981b98f4a7a2a99df5c042
Savinay Kumar <epicsark@gmail.com>
2022-12-10 21:48:53 +0530

    Revert "updated App.js"
    This reverts commit d481ab86134e604a29a20c38712e81290670a7c3.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi, Welcome to My React Website!!!
+          Hi, Welcome!!!
         </p>
         <a
           className="App-link"
