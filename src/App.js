3c108b55470240814ce55c371ce8a6ab0cf08402
Savinay Kumar <epicsark@gmail.com>
2022-12-10 22:22:14 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Savinay Kumar
+          Savinay Kumar, Full Stack Developer
         </a>
       </header>
     </div>
