218b5a00ce235055111e4a76cade8d99d73f9736
Savinay Kumar <epicsark@gmail.com>
2022-12-10 21:59:16 +0530

    updated hello msg

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi, Welcome!!!
+          Hi, Welcome to my website!
         </p>
         <a
           className="App-link"
